93743849096a450e17f9f78ba1736c462911b1f6
SirSerje <sir.serje@gmail.com>
2020-03-20 20:23:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './styles.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      favorites: [
+        { id: new Date().getTime(), name: new Date().getTime() },
+        { id: new Date().getTime() + 1, name: new Date().getTime() + 1 }],
+    }
+  }
+
+  handleAdd = (e) => {
+    const items = this.state.favorites;
+    this.setState({ favorites: [...items, { id: new Date().getTime(), name: new Date().getTime() }] })
+  };
+
+  handleRemove = (e) => {
+    const items = this.state.favorites.filter(i => +e.target.id !== i.id);
+    this.setState({ favorites: items })
+  };
+
+  render() {
+    console.log(this.state.favorites);
+    return (
+      <div className="favorites">
+        <Button onClick={this.handleAdd}>add</Button>
+        <TransitionGroup component="ul">
+          {this.state.favorites.map(
+            ({ id, name }) => (
+              <CSSTransition
+                timeout={500}
+                classNames="fade"
+                key={id}
+              >
+                <li className="favorite">
+                  {name}
+                  <Button id={id} onClick={this.handleRemove}>remove</Button>
+                </li>
+              </CSSTransition>
+            ),
+          )}
+        </TransitionGroup>
+      </div>
+    );
+  }
 }
-
-export default App;
